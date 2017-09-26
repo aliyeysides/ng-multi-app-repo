5aa32f61aeb1d45a9e023d467ffd4c6b7bb51d46
ali yeysides <ali.yeysides@gmail.com>
2017-09-26 13:34:26 -0400

    adds environments

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  envName: 'demo',
+  envProtocol: 'https',
+  envHostName: 'demo.chaikinanalytics.com'
+};
---
+++ src/environments/environment.local.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  envName: 'local',
+  envProtocol: 'http',
+  envHostName: 'localhost:8080',
+};
