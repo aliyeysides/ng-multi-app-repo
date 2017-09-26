1a0fdc0aa432ddeec2ead5b768553b831451b35d
ali yeysides <ali.yeysides@gmail.com>
2017-09-26 16:03:27 -0400

    fixes global scss var paths

--- src/app/containers/stock-report/report/report.component.ts
+++ src/app/containers/stock-report/report/report.component.ts
@@ -1,19 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {environment} from '../../../../environments/environment';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'cpt-report',
   template: `
-    <p>
-      report Works!
-    </p>
+    <div class="stock-view__container container">
+      <div class="row--header header text-center row no-gutters">
+        <h1 class="col-12"><a (click)="goBack()" class="back"><i class="fa fa-reply" aria-hidden="true"></i></a> Stock
+          Report for</h1>
+      </div>
+      <iframe class="stock-view__iframe" id="iframeId" [src]="sanitizedSrc"></iframe>
+    </div>
   `,
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
+  public symbol: string;
+  public src: string;
+  public sanitizedSrc: SafeUrl;
 
-  constructor() { }
+  apiHostName = environment.envProtocol + '://' + environment.envHostName;
+
+  constructor(private route: ActivatedRoute,
+              private sanitizer: DomSanitizer,
+              private location: Location) {
+  }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(params => {
+          if (params.symbol) {
+            this.symbol = params.symbol;
+          } else {
+            this.symbol = 'AAPL';
+          }
+          this.src = `${this.apiHostName}/CPTRestSecure/StockSummary/index.html?lang=English&uid=9582&environment=desktop&subEnvironment=chaikinAnalytics&version=1.3.2&symbol=${this.symbol}&userType=CAUser`;
+          this.sanitizedSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.src);
+        }
+      );
   }
+
+  goBack() {
+    this.location.back();
+  }
+
 
 }
--- src/app/containers/stock-report/stock-report.routing.ts
+++ src/app/containers/stock-report/stock-report.routing.ts
@@ -4,5 +4,6 @@ import {RouterModule} from '@angular/router';
 import {StockReportComponent} from './stock-report.component';
 
 export const routing: ModuleWithProviders = RouterModule.forChild([
-  { path: 'report', component: StockReportComponent }
+  { path: 'report', redirectTo: 'report/AAPL' },
+  { path: 'report/:symbol', component: StockReportComponent }
 ]);
