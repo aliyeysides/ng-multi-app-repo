24c231da87885d4880cfe00383fbd2a12dfe1eae
C Belluso <cbelluso@Christians-iMac.local>
2017-09-27 15:07:07 -0400

    side panels

--- src/app/containers/dashboard/bear-of-the-week/bear-of-the-week.component.ts
+++ src/app/containers/dashboard/bear-of-the-week/bear-of-the-week.component.ts
@@ -3,9 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'cpt-bear-of-the-week',
   template: `
-    <p>
-      bear-of-the-week Works!
-    </p>
+    <div class="insights__container insights__container--small">
+      <div class="post-head post-head--bearpick">
+        <h4>Bear of the Week</h4>
+        <div class="divider-h"></div>
+        <p class="header__post-date">Sept 27, 2017</p>
+        <a class="post-head__button">
+          <i class="fa fa-calendar" aria-hidden="true"></i>
+           <span>&nbsp;Previous</span>
+        </a>
+      </div>
+      <div class="post-body post-body--bearpick">
+      </div>
+    </div>
   `,
   styleUrls: ['./bear-of-the-week.component.scss']
 })
