f8ca6f6ba02f68dd481db56c81c6f7e3fd291076
ali yeysides <ali.yeysides@gmail.com>
2017-09-28 11:34:46 -0400

    adds signal service and discovery stock component

--- src/app/containers/discovery/discovery.component.ts
+++ src/app/containers/discovery/discovery.component.ts
@@ -2,12 +2,28 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DiscoveryService} from '../../core/services/discovery.service';
 import {Subject} from 'rxjs/Subject';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Idea} from '../../shared/models/idea';
 
 @Component({
   selector: 'cpt-discovery',
   template: `
-    <div class="container discovery">
-      <cpt-discovery-seed></cpt-discovery-seed>
+    <div class="container-fluid">
+      <div class="discovery__body body">
+        <div class="body__top">
+          <div class="row--header header text-center row no-gutters">
+            <h1 class="col-12">Discovery Results for</h1>
+          </div>
+          <cpt-discovery-seed (addToListClicked)="addToList($event)"
+                               (viewStockReportClicked)="viewStockReport($event)"
+                               [metaInfo]="metaInfo"></cpt-discovery-seed>
+        </div>
+        <div class="body__bottom">
+          <!--<app-discovery-results (addToListClicked)="addToList($event)"-->
+                                 <!--(viewStockReportClicked)="viewStockReport($event)"-->
+                                 <!--(viewStockDiscoveryClicked)="viewStockDiscovery($event)"-->
+                                 <!--[results]="results"></app-discovery-results>-->
+        </div>
+      </div>
     </div>
   `,
   styleUrls: ['./discovery.component.scss']
@@ -15,7 +31,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class DiscoveryComponent implements OnInit, OnDestroy {
 
   private ngUnsubscribe: Subject<void> = new Subject<void>();
-  // public metaInfo: Idea;
+  public metaInfo: Idea;
   // public results: object[];
 
   constructor(private router: Router,
@@ -51,7 +67,7 @@ export class DiscoveryComponent implements OnInit, OnDestroy {
   // }
 
   private parseDiscoveryResponse(res: object) {
-    // this.metaInfo = res['metainfo'] as Idea;
+    this.metaInfo = res['metainfo'] as Idea;
     // this.results = res['results'];
   }
 
--- src/app/containers/discovery/index.ts
+++ src/app/containers/discovery/index.ts
@@ -3,17 +3,20 @@ import {DiscoveryComponent} from './discovery.component';
 import {routing} from './discovery.routing';
 import {DiscoverySeedComponent} from './discovery-seed/discovery-seed.component';
 import {DiscoveryService} from '../../core/services/discovery.service';
+import {SharedModule} from '../../shared/index';
+import {SignalService} from '../../core/services/signal.service';
 
 @NgModule({
   imports: [
-    routing
+    routing,
+    SharedModule
   ],
   exports: [DiscoveryComponent],
   declarations: [
     DiscoveryComponent,
     DiscoverySeedComponent
   ],
-  providers: [DiscoveryService]
+  providers: [DiscoveryService, SignalService]
 })
 export class DiscoveryModule {
 }
