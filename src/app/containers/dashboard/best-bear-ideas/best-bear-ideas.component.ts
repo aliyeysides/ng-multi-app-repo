3c5916f0555be493e9113fd8b9b549adf2d6416d
C Belluso <cbelluso@Christians-iMac.local>
2017-09-28 11:13:27 -0400

    added content to side containers

--- src/app/containers/dashboard/best-bear-ideas/best-bear-ideas.component.ts
+++ src/app/containers/dashboard/best-bear-ideas/best-bear-ideas.component.ts
@@ -12,15 +12,130 @@ import { Component, OnInit } from '@angular/core';
              <span>&nbsp;View List</span>
           </a>
         </div>
-        <div class="col-header__container row">
-          <div class="col-header col-sm-2">Rating</div>
-          <div class="col-header col-header--ticker col-sm-4">Ticker</div>
-          <div class="col-header col-sm-3">Last Price</div>
-          <div class="col-header col-sm-3">% Chg</div>
+        <div class="col-header__container row no-gutters">
+          <div class="col-header col-2">Rating</div>
+          <div class="col-header col-header--ticker col-3">Ticker</div>
+          <div class="col-header col-1"></div>
+          <div class="col-header col-3">Last Price</div>
+          <div class="col-header col-3">% Chg</div>
         </div>
       </div>
-      <div class="post-body post-body--list">
-      </div>
+      <ul class="post-body post-body--bearlist">
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_VeryBearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">MAT</p>
+          </div>
+          <div class="col-1 stock__alert">
+            <i class="fa fa-play down-change down-alert" aria-hidden="true"></i>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">$455.55</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">+10.54%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_VeryBearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">TSLA</p>
+          </div>
+          <div class="col-1 stock__alert">
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data down-change">$356.77</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data down-change">-3.33%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_Bearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">AMZN</p>
+          </div>
+          <div class="col-1 stock__alert">
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">$76.28</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">+1.01%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_Bearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">OXY</p>
+          </div>
+          <div class="col-1 stock__alert">
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data down-change">$554.32</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data down-change">-2.99%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_Bearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">MSFT</p>
+          </div>
+          <div class="col-1 stock__alert">
+            <i class="fa fa-play up-change up-alert" aria-hidden="true"></i>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">$455.55</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">+10.54%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_VeryBearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">CMCSA</p>
+          </div>
+          <div class="col-1 stock__alert">
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">$455.55</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">+10.54%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_VeryBearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">CMCSA</p>
+          </div>
+          <div class="col-1 stock__alert">
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">$455.55</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">+10.54%</p>
+          </div>
+        </li>
+      </ul>
     </div>
   `,
   styleUrls: ['./best-bear-ideas.component.scss']
--- src/app/containers/dashboard/user-lists/user-lists.component.ts
+++ src/app/containers/dashboard/user-lists/user-lists.component.ts
@@ -16,32 +16,133 @@ import { Component, OnInit } from '@angular/core';
              <span>&nbsp;View List</span>
           </a>
         </div>
-        <div class="col-header__container row">
-          <div class="col-header col-sm-2">Rating</div>
-          <div class="col-header col-header--ticker col-sm-4">Ticker</div>
-          <div class="col-header col-sm-3">Last Price</div>
-          <div class="col-header col-sm-3">% Chg</div>
+        <div class="col-header__container row no-gutters">
+          <div class="col-header col-2">Rating</div>
+          <div class="col-header col-header--ticker col-3">Ticker</div>
+          <div class="col-header col-1"></div>
+          <div class="col-header col-3">Last Price</div>
+          <div class="col-header col-3">% Chg</div>
         </div>
       </div>
-      <div class="post-body post-body--list">
-        <div class="row">
-          <div class="col-lg-2 col-xl-2 stock__PGR">
-            <img class="align-absolute" src="/.svg">
+      <ul class="post-body post-body--userlist">
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_VeryBullish.svg">
           </div>
-          <div class="col-lg-3 col-xl-3 stock__ticker">
+          <div class="col-3 stock__ticker">
             <p class="ticker">CMCSA</p>
           </div>
-          <div class="col-lg-1 col-xl-1 stock__alert">
-            <i class="fa fa-bell" aria-hidden="true"></i>
+          <div class="col-1 stock__alert">
+            <i class="fa fa-bell up-change" aria-hidden="true"></i>
           </div>
-          <div class="col-lg-3 col-xl-3 stock__price">
-            <p class="data">$455.55</p>
+          <div class="col-3 stock__price">
+            <p class="data up-change">$455.55</p>
           </div>
-          <div class="col-lg-3 col-xl-3 stock__price">
-            <p class="data">+10.54%</p>
+          <div class="col-3 stock__price">
+            <p class="data up-change">+10.54%</p>
           </div>
-        </div>
-      </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_VeryBullish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">T</p>
+          </div>
+          <div class="col-1 stock__alert">
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data down-change">$356.77</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data down-change">-3.33%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_Bearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">YUM</p>
+          </div>
+          <div class="col-1 stock__alert">
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">$76.28</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">+1.01%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_VeryBullish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">AMZN</p>
+          </div>
+          <div class="col-1 stock__alert">
+            <i class="fa fa-bell down-change" aria-hidden="true"></i>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data down-change">$554.32</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data down-change">-2.99%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_VeryBearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">CMCSA</p>
+          </div>
+          <div class="col-1 stock__alert">
+            <i class="fa fa-bell up-change" aria-hidden="true"></i>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">$455.55</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">+10.54%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_VeryBearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">CMCSA</p>
+          </div>
+          <div class="col-1 stock__alert">
+            <i class="fa fa-bell up-change" aria-hidden="true"></i>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">$455.55</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">+10.54%</p>
+          </div>
+        </li>
+        <li class="row no-gutters">
+          <div class="col-2 stock__PGR">
+            <img class="align-absolute" src="../../assets/imgs/arc_VeryBearish.svg">
+          </div>
+          <div class="col-3 stock__ticker">
+            <p class="ticker">CMCSA</p>
+          </div>
+          <div class="col-1 stock__alert">
+            <i class="fa fa-bell up-change" aria-hidden="true"></i>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">$455.55</p>
+          </div>
+          <div class="col-3 stock__price">
+            <p class="data up-change">+10.54%</p>
+          </div>
+        </li>
+      </ul>
     </div>
   `,
   styleUrls: ['./user-lists.component.scss']
