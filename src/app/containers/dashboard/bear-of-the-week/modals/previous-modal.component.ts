e63b9cfbcea466fbd9e7a0050b24ad12fc408ae1
C Belluso <cbelluso@Christians-iMac.local>
2017-10-17 11:43:23 -0400

    quick fix

--- src/app/containers/dashboard/bear-of-the-week/modals/previous-modal.component.ts
+++ src/app/containers/dashboard/bear-of-the-week/modals/previous-modal.component.ts
@@ -31,12 +31,12 @@ import {Observable} from 'rxjs/Observable';
               </div>
               <div class="col-6">
                 <p class="company">{{ post['data']['meta-info'].name }}</p>
-              </div>
+              </div> 
               <div class="col-6">
                 <p class="pick-date">{{ post['post_date_formatted'] }}</p>
               </div>
               <div class="col-6">
-                <p class="pick-date">{{ post['post_date_formatted'] }}</p>
+                <p class="industry">{{ post['data']['meta-info'].industry }}</p>
               </div>
               <div class="col-12">
                 <a (click)="openCommentaryModal(post)">Commentary &nbsp;<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
