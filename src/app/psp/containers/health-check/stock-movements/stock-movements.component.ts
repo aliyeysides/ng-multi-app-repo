3092c88ff60b56ee52285012e61e2ae0cfd17233
C Belluso <cbelluso@Christians-iMac.local>
2017-12-01 15:20:00 -0500

    done

--- src/app/psp/containers/health-check/stock-movements/stock-movements.component.ts
+++ src/app/psp/containers/health-check/stock-movements/stock-movements.component.ts
@@ -13,6 +13,13 @@ import {SignalService} from '../../../../services/signal.service';
   template: `
     <div id="HC--Stock-Movements" class="col-12 col-lg-4 col-xl-4 float-lg-left">
 
+      <div class="row section__toggle">
+        <div class="col-12 toggle toggle--timespan">
+          <p class="toggle__left selected">TODAY</p>
+          <p class="toggle__right">LAST WEEK</p>
+        </div>
+      </div>
+
       <div class="row">
         <div class="col-12">
           <h2>Stock Movements</h2>
