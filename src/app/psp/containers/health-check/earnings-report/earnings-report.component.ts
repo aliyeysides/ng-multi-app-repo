c754a71cb9599e2eaa6bbd57b86e5a6c591addd4
C Belluso <cbelluso@Christians-iMac.local>
2017-12-27 17:03:49 -0500

    pounchlist

--- src/app/psp/containers/health-check/earnings-report/earnings-report.component.ts
+++ src/app/psp/containers/health-check/earnings-report/earnings-report.component.ts
@@ -24,25 +24,25 @@ interface EarningsReportObj {
 
       <div class="row">
         <div class="col-12">
-          <h2>Earnings Reports</h2>
+          <h2>Earnings Info</h2>
         </div>
       </div>
 
       <div class="row section__summary justify-content-center">
         <div class="col-6 col-md-5 summary--left">
-          <p><img src="./assets/imgs/icon_circle-earnings--green.svg">{{ upCount }}</p>
+          <p><img src="./assets/imgs/icon_circle-arrow--green.svg">{{ upCount }}</p>
         </div>
         <div class="col-6 col-md-5 summary--right">
-          <p><img src="./assets/imgs/icon_circle-earnings--red.svg">{{ downCount }}</p>
+          <p><img src="./assets/imgs/icon_circle-arrow--red.svg">{{ downCount }}</p>
         </div>
       </div>
 
       <div *ngIf="!collapse" class="row">
 
         <div class="col-12 col-md-6">
-          <div class="row">
+          <div class="row" style="min-height:10px;">
             <div class="col-5"></div>
-            <div class="col-2">
+            <div class="col-2 hidden-md-up">
               <span class="icon__separator"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M400 64h-48V12c0-6.6-5.4-12-12-12h-8c-6.6 0-12 5.4-12 12v52H128V12c0-6.6-5.4-12-12-12h-8c-6.6 0-12 5.4-12 12v52H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM48 96h352c8.8 0 16 7.2 16 16v48H32v-48c0-8.8 7.2-16 16-16zm352 384H48c-8.8 0-16-7.2-16-16V192h384v272c0 8.8-7.2 16-16 16zM212.7 224h22.6c6.9 0 12.4 5.8 12 12.7l-6.7 120c-.4 6.4-5.6 11.3-12 11.3h-9.3c-6.4 0-11.6-5-12-11.3l-6.7-120c-.3-6.9 5.2-12.7 12.1-12.7zM252 416c0 15.5-12.5 28-28 28s-28-12.5-28-28 12.5-28 28-28 28 12.5 28 28z"/></svg></span></div>
             <div class="col-5"></div>
           </div>
@@ -88,9 +88,9 @@ interface EarningsReportObj {
         </div>
         
         <div class="col-12 col-md-6">
-          <div class="row">
+          <div class="row" style="min-height:10px;">
             <div class="col-5"></div>
-            <div class="col-2">
+            <div class="col-2 hidden-md-up">
               <span class="icon__separator"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M400 64h-48V12c0-6.6-5.4-12-12-12h-8c-6.6 0-12 5.4-12 12v52H128V12c0-6.6-5.4-12-12-12h-8c-6.6 0-12 5.4-12 12v52H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM48 96h352c8.8 0 16 7.2 16 16v48H32v-48c0-8.8 7.2-16 16-16zm352 384H48c-8.8 0-16-7.2-16-16V192h384v272c0 8.8-7.2 16-16 16zM255.7 269.7l34.6 34.6c2.1 2.1 2.1 5.4 0 7.4L159.1 442.9l-35.1 5c-6.9 1-12.9-4.9-11.9-11.9l5-35.1 131.2-131.2c2-2 5.4-2 7.4 0zm75.2 1.4l-19.2 19.2c-2.1 2.1-5.4 2.1-7.4 0l-34.6-34.6c-2.1-2.1-2.1-5.4 0-7.4l19.2-19.2c6.8-6.8 17.9-6.8 24.7 0l17.3 17.3c6.8 6.8 6.8 17.9 0 24.7z"/></svg></span>
             </div>
             <div class="col-5"></div>
@@ -144,11 +144,11 @@ interface EarningsReportObj {
       <div class="row">
         <div *ngIf="!collapse" (click)="toggleCollapse()" class="col-12 expand-collapse">
           <img src="./assets/imgs/ux__collapse--circle.svg">
-          <p>COLLAPSE</p>
+          <p>Collapse</p>
         </div>
         <div *ngIf="collapse" (click)="toggleCollapse()" class="col-12 expand-collapse">
           <img src="./assets/imgs/ux__expand--circle.svg">
-          <p>EXPAND</p>
+          <p>Expand for detail</p>
         </div>
       </div>
 
