9e36ecbe083d31a5bc39be4dcc176f27c704a0a5
ali yeysides <ali.yeysides@gmail.com>
2018-01-05 14:40:01 -0500

    sets up pagination in onboarding

--- src/app/psp/core/psp-onboarding/psp-onboarding.component.ts
+++ src/app/psp/core/psp-onboarding/psp-onboarding.component.ts
@@ -11,21 +11,42 @@ import {BsModalRef} from 'ngx-bootstrap';
       </button>
     </div>
     <div class="modal-body">
-      <!--<ul *ngIf="list.length">-->
-        <!--<li *ngFor="let item of list">{{item}}</li>-->
-      <!--</ul>-->
+      <div *ngIf="items.length">
+        <ng-container *ngFor="let item of items">
+          <ng-container *ngIf="currentPage === item.id">
+            <img src="{{ item.src }}">
+          </ng-container>
+        </ng-container>
+      </div>
     </div>
     <div class="modal-footer">
-      <!--<button type="button" class="btn btn-default" (click)="bsModalRef.hide()">Close</button>-->
+      <div class="col-xs-12 col-12">
+        <pagination [itemsPerPage]="itemsPerPage" [totalItems]="totalItems" [(ngModel)]="currentPage"></pagination>
+      </div>
     </div>
   `,
   styleUrls: ['./psp-onboarding.component.scss']
 })
 export class PspOnboardingComponent implements OnInit {
+  totalItems: number;
+  itemsPerPage = 1;
+  currentPage = 1;
+  items: Array<object> = [
+    {
+      id: 1,
+      src: 'assets/imgs/screen-insights-100217-1.png'
+    },
+    {
+      id: 2,
+      src: 'assets/imgs/screen-insights-100217-2.png'
+    }
+  ];
 
-  constructor(public bsModalRef: BsModalRef) {}
+  constructor(public bsModalRef: BsModalRef) {
+  }
 
   ngOnInit() {
+    this.totalItems = this.items.length;
   }
 
 }
