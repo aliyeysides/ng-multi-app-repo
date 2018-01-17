c0ce67e545695296edf5e8a4b54d0658470eef22
C Belluso <cbelluso@Christians-iMac.local>
2018-01-17 11:38:18 -0500

    hotfix

--- src/app/psp/core/psp-onboarding/psp-onboarding.component.ts
+++ src/app/psp/core/psp-onboarding/psp-onboarding.component.ts
@@ -17,8 +17,8 @@ import {BsModalRef} from 'ngx-bootstrap';
       <div *ngIf="items.length">
         <ng-container *ngFor="let item of items">
           <ng-container *ngIf="currentPage === item.id">
-            <img src="{{ item.src }}">
-            <img *ngIf="item.mobileSrc" src="{{ item.mobileSrc }}">
+            <img class="desktop hidden-sm-down" src="{{ item.src }}">
+            <img class="mobile hidden-sm-up" *ngIf="item.mobileSrc" src="{{ item.mobileSrc }}">
             <a class="link__watch-video" target="_blank" *ngIf="item.link" href="item.link"><i class="far fa-play-circle"></i> &nbsp; WATCH THE VIDEO</a>
           </ng-container>
         </ng-container>
@@ -29,7 +29,7 @@ import {BsModalRef} from 'ngx-bootstrap';
         <pagination class="align-absolute" [itemsPerPage]="itemsPerPage" [totalItems]="totalItems" [(ngModel)]="currentPage"></pagination>
       </div>
       <ng-container *ngIf="currentPage === ( totalItems / itemsPerPage )">
-        <a class="link__get-started" (click)="bsModalRef.hide()">Get Started</a>
+        <a class="link__get-started" (click)="bsModalRef.hide()">Start!</a>
       </ng-container>
     </div>
   `,
@@ -43,25 +43,29 @@ export class PspOnboardingComponent implements OnInit {
     {
       id: 1,
       src: 'assets/imgs/img_onboard-screen1.png',
-      mobileSrc: 'assets/imgs/img_onboard-screen2.png'
+      mobileSrc: 'assets/imgs/img_onboard-screen1--mobile.png'
     },
     {
       id: 2,
       src: 'assets/imgs/img_onboard-screen2.png',
+      mobileSrc: 'assets/imgs/img_onboard-screen2--mobile.png'
     },
     {
       id: 3,
       src: 'assets/imgs/img_onboard-screen3.png',
+      mobileSrc: 'assets/imgs/img_onboard-screen3--mobile.png',
       link: 'https://www.chaikinanalytics.com/powerpulse-health-check-video/'
     },
     {
       id: 4,
       src: 'assets/imgs/img_onboard-screen4.png',
+      mobileSrc: 'assets/imgs/img_onboard-screen4--mobile.png',
       link: 'https://www.chaikinanalytics.com/powerpulse-stock-analysis/'
     },
     {
       id: 5,
       src: 'assets/imgs/img_onboard-screen5.png',
+      mobileSrc: 'assets/imgs/img_onboard-screen5--mobile.png',
       link: 'https://www.chaikinanalytics.com/powerpulse-market-insights-video/'
     },
   ];
