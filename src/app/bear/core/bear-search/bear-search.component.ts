29f55d2ac9e1d90bacc24aef4468845e9ca7ee3e
ali yeysides <ali.yeysides@gmail.com>
2018-01-22 15:19:27 -0500

    adds tooltip module

--- src/app/bear/core/bear-search/bear-search.component.ts
+++ src/app/bear/core/bear-search/bear-search.component.ts
@@ -8,7 +8,7 @@ declare let gtag: Function;
 @Component({
   selector: 'cpt-bear-search',
   template: `
-    <a class="quick-link">
+    <a tooltip="Search" placement="auto" class="quick-link">
       <svg class="align-absolute" width="300px" height="300px" viewBox="0 0 300 300" version="1.1"
            xmlns="http://www.w3.org/2000/svg"
            xmlns:xlink="http://www.w3.org/1999/xlink">
--- src/app/bear/core/bear-search/index.ts
+++ src/app/bear/core/bear-search/index.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {BearSearchComponent} from './bear-search.component';
 import {SharedModule} from '../../../shared/index';
 import {SymbolSearchService} from '../../../services/symbol-search.service';
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    TooltipModule
   ],
   exports: [BearSearchComponent],
   declarations: [BearSearchComponent],
