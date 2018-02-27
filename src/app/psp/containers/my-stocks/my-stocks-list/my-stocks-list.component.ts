e42ea8b33491abea4184345157215d32c0ccb419
ali yeysides <ali.yeysides@gmail.com>
2018-02-27 15:35:34 -0500

    fix

--- src/app/psp/containers/my-stocks/my-stocks-list/my-stocks-list.component.ts
+++ src/app/psp/containers/my-stocks/my-stocks-list/my-stocks-list.component.ts
@@ -137,19 +137,23 @@ export class MyStocksListComponent implements OnInit, OnDestroy {
   private _userlists: BehaviorSubject<object[]> = new BehaviorSubject<object[]>({} as object[]);
 
   @HostListener('window:keydown', ['$event']) onKeyDown(e) {
-    e.preventDefault();
-    e.stopPropagation();
+    const valid = ['Tab', 'ArrowUp', 'ArrowDown'];
     const ordArr = this.orderBy.transform(this.myStocks, 'PGR', false);
     const indx = ordArr.map(x => x.symbol).indexOf(this.selectedStock);
-    if (e.key === 'Tab') {
-      const next = ordArr.map(x => x.symbol).indexOf(this.selectedStock) + 1;
-      if (ordArr.length > next) this.selectStock(ordArr[next].symbol);
-    }
-    if (e.key === "ArrowUp") {
-      if (indx-1 >= 0) this.selectStock(ordArr[indx-1].symbol);
-    }
-    if (e.key === "ArrowDown") {
-      if (ordArr.length > indx+1) this.selectStock(ordArr[indx+1].symbol);
+
+    if (valid.filter(x => x === e.key).length > 0) {
+      e.preventDefault();
+      e.stopPropagation();
+      if (e.key === 'Tab') {
+        const next = ordArr.map(x => x.symbol).indexOf(this.selectedStock) + 1;
+        if (ordArr.length > next) this.selectStock(ordArr[next].symbol);
+      }
+      if (e.key === "ArrowUp") {
+        if (indx-1 >= 0) this.selectStock(ordArr[indx-1].symbol);
+      }
+      if (e.key === "ArrowDown") {
+        if (ordArr.length > indx+1) this.selectStock(ordArr[indx+1].symbol);
+      }
     }
   }
 
