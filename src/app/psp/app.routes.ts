6e4eeeddc77aa913e404ca3931a5ed1a60063e6f
ali yeysides <ali.yeysides@gmail.com>
2018-03-07 14:59:46 -0500

    refactor

--- src/app/psp/app.routes.ts
+++ src/app/psp/app.routes.ts
@@ -1,4 +1,4 @@
-import {Routes, RouterModule} from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const ROUTES: Routes = [
   {path: '', redirectTo: 'health-check', pathMatch: 'full'},
--- src/app/psp/containers/health-check/health-check.component.ts
+++ src/app/psp/containers/health-check/health-check.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit, EventEmitter, Output} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HealthCheckService} from '../../../services/health-check.service';
 import {AuthService} from '../../../services/auth.service';
 
