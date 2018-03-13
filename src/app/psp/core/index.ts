4ad07f72efebc44095fd7d929536c5244227a3ab
ali yeysides <ali.yeysides@gmail.com>
2018-03-13 14:00:07 -0400

    refactors psp symbol search

--- src/app/psp/core/index.ts
+++ src/app/psp/core/index.ts
@@ -1,9 +1,11 @@
 import {PspSettingsModule} from './psp-settings-menu';
 import {PspNavigatorModule} from './psp-navigator';
 import {PspMarketSummaryModule} from './market-summary';
+import {PspSymbolSearchModule} from './psp-symbol-search';
 
 export const APP_CORE_MODULES = [
   PspSettingsModule,
   PspNavigatorModule,
-  PspMarketSummaryModule
+  PspMarketSummaryModule,
+  PspSymbolSearchModule
 ];
